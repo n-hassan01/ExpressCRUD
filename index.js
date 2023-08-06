8c8afcace64fe4ce009b88dbd838d888dcab464f
n-hassan01 <naimul.hassan.se@gmail.com>
2023-08-06 17:49:59 +0600

    adding input validation and complete crud

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -32,12 +33,58 @@ app.get('/remark/api/user/:id', (req, res) => {
 });
 
 app.post('/remark/api/user/add', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        phone: Joi.string().min(11)
+    });
+    const validation = schema.validate(req.body);
+
+    if(validation.error) {
+        res.status(400).send("Invalid inputs");
+        return;
+    }
+
     const user = {
         id: users.length + 1,
         name: req.body.name,
         phone: req.body.phone
     };
     users.push(user);
+
+    res.send(user);
+});
+
+app.delete('/remark/api/user/delete/:id', (req, res) => {
+    const user = users.find(c => c.id === parseInt(req.params.id));
+    if(!user) res.status(404).send('User not found for the given id');
+
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+
+    res.send(users);
+});
+
+app.put('/remark/api/user/update/:id', (req, res) => {
+    const schema = Joi.object({
+        phone: Joi.string().min(11)
+    });
+    const validation = schema.validate(req.body);
+
+    if(validation.error) {
+        res.status(400).send("Invalid inputs");
+        return;
+    }
+
+    const user = users.find(c => c.id === parseInt(req.params.id));
+    if(!user) res.status(404).send('User not found for the given id');
+
+    if(req.body.name) {
+        user.name = req.body.name;
+    }
+
+    if(req.body.phone) {
+        user.phone = req.body.phone;
+    }
 
     res.send(user);
 });
