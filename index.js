892aa7983ce5f6250cd3f0573a83dd40e5ee8d1d
n-hassan01 <naimul.hassan.se@gmail.com>
2023-08-06 16:29:25 +0600

    adding search user and add user apis

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 let users = [
     {
-        "id": 1,
-        "name": "Niloy",
-        "phone": "01521888"
+        id: 1,
+        name: "Niloy",
+        phone: "01521888"
     },
     {
-        "id": 2,
-        "name": "Antora",
-        "phone": "01513677"
+        id: 2,
+        name: "Antora",
+        phone: "01513677"
     },
     {
-        "id": 3,
-        "name": "Rakib",
-        "phone": "017223899"
+        id: 3,
+        name: "Rakib",
+        phone: "017223899"
     }
 ]
 
 app.get('/remark/api/users', (req, res) => {
     res.send(users);
+});
+
+app.get('/remark/api/user/:id', (req, res) => {
+    const user = users.find(c => c.id === parseInt(req.params.id));
+    if(!user) res.status(404).send('User not found for the given id');
+    res.send(user);
+});
+
+app.post('/remark/api/user/add', (req, res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name,
+        phone: req.body.phone
+    };
+    users.push(user);
+
+    res.send(user);
 });
 
 // environment variable
