09fe45cadf0d9cea468b67d25d685784a0d2ad9c
n-hassan01 <naimul.hassan.se@gmail.com>
2023-08-10 11:44:00 +0600

    error handling

--- updateUser.js
+++ updateUser.js
@@ -9,22 +9,40 @@ router.put('/:id', (req, res) => {
     const { name, designation } = req.body;
 
     if (name && !designation) {
-        pool.query('UPDATE employee SET name = $1, WHERE id = $2', [name, id], (error) => {
-            if (error) throw error;
+        pool.query('UPDATE employee SET name = $1 WHERE id = $2', [name, id], (error) => {
+            try {
+                if (error) throw error;
 
-            res.send('Successfully updated the information of the employee!');
+                res.send('Successfully updated the information of the employee!');
+            // eslint-disable-next-line no-shadow
+            } catch (error) {
+                console.log(error.message);
+                res.status(500).send(error.message);
+            }
         });
     } else if (designation && !name) {
         pool.query('UPDATE employee SET designation = $1 WHERE id = $2', [designation, id], (error) => {
-            if (error) throw error;
+            try {
+                if (error) throw error;
 
-            res.send('Successfully updated the information of the employee!');
+                res.send('Successfully updated the information of the employee!');
+            // eslint-disable-next-line no-shadow
+            } catch (error) {
+                console.log(error.message);
+                res.status(500).send(error.message);
+            }
         });
     } else if (name && designation) {
         pool.query('UPDATE employee SET name = $1, designation = $2 WHERE id = $3', [name, designation, id], (error) => {
-            if (error) throw error;
+            try {
+                if (error) throw error;
 
-            res.send('Successfully updated the information of the employee!');
+                res.send('Successfully updated the information of the employee!');
+            // eslint-disable-next-line no-shadow
+            } catch (error) {
+                console.log(error.message);
+                res.status(500).send(error.message);
+            }
         });
     }
 });
